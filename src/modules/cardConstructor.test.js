1a8975b35f1345d771ee84569d70718d952066dd
malcomcrab <malcomcrabesq@outlook.com>
2025-03-18 16:22:21 +0000

    Test: Adds test file and four tests for cardConstructor module.

---
+++ src/modules/cardConstructor.test.js
@@ -0,0 +1,28 @@
+import { it, describe, expect} from 'vitest';
+import { PokemonCardObject } from './cardConstructor';
+
+describe('PokemonCardObject Contructor', () => {
+  
+  const newCard = new PokemonCardObject('Tom', 'url', 1, 'grass')
+
+  it('Should return am object', () => {
+    expect(newCard).toBeTypeOf("object")  
+  });
+
+  it('Should return an object containing input arguments', () => {
+    expect(newCard.name).toStrictEqual('Tom')
+    expect(newCard.imageUrl).toStrictEqual('url')
+    expect(newCard.id).toStrictEqual(1)
+    expect(newCard.type).toStrictEqual('grass')
+  })
+
+  it('Should initially return isClicked as false', () => {
+    expect(newCard.isClicked).toBe(false)  
+  });
+
+  it('Should store isClicked as true when updated', () => {
+    newCard.isClicked = true
+    expect(newCard.isClicked).toBe(true)  
+  });
+
+});
--- src/modules/shuffleArray.test.js
+++ src/modules/shuffleArray.test.js
@@ -3,23 +3,22 @@ import { shuffleArray } from "./shuffleArray";
 
 describe('shuffleArray', () => {
 
-it("Should return an Array", () => {
-    const testArray = [1,2,3,4,5]
-    const shuffled = shuffleArray(testArray)
-    expect(Array.isArray(shuffled)).toBe(true)
-})
+    it("Should return an Array", () => {
+        const testArray = [1,2,3,4,5]
+        const shuffled = shuffleArray(testArray)
+        expect(Array.isArray(shuffled)).toBe(true)
+    })
 
-it("Should fail if anything other than an Array is returned", () => {
-    const testArray = [1,2,3,4,5]
-    const shuffled = shuffleArray(testArray)
-    expect(Array.isArray(shuffled)).not.toBe(false)
-})
+    it("Should fail if anything other than an Array is returned", () => {
+        const testArray = [1,2,3,4,5]
+        const shuffled = shuffleArray(testArray)
+        expect(Array.isArray(shuffled)).not.toBe(false)
+    })
 
-it("Should return an Array the same length as passed in", () => {
-    const testArray = [1,2,3,4,5]
-    const shuffled = shuffleArray(testArray)
-    expect(shuffled.length).toEqual(testArray.length)
-
-})
+    it("Should return an Array the same length as passed in", () => {
+        const testArray = [1,2,3,4,5]
+        const shuffled = shuffleArray(testArray)
+        expect(shuffled.length).toEqual(testArray.length)
+    })
 
 })
