99a3964d6386e73ec43a9f0fc3387d960107555b
malcomcrab <malcomcrabesq@outlook.com>
2025-03-18 17:22:28 +0000

    Style: tidied scoreboard test file

--- src/components/GameComponents/ScoreBoard.jsx
+++ src/components/GameComponents/ScoreBoard.jsx
@@ -3,8 +3,8 @@ import styles from "../../cssModules/scoreBoard.module.css"
 function ScoreBoard({ currentScore, highScore }) {
   return (
     <div className={styles.scoreBoardContainer}>
-      <p role="currentScore">Current Score: {currentScore}</p>
-      <p role="highScore">High Score: {highScore}</p>
+      <p data-testid="currentScore" role="currentScore">Current Score: {currentScore}</p>
+      <p data-testid="highScore">High Score: {highScore}</p>
     </div>
   );
 }
--- src/components/GameComponents/scoreboard.test.jsx
+++ src/components/GameComponents/scoreboard.test.jsx
@@ -1,31 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-
 import ScoreBoard from './ScoreBoard';
 
 describe('Scoreboard', () => {
   it('renders currentScore passed as props', () => {
     render(<ScoreBoard currentScore={1} />);
     
-    expect(screen.getByRole("currentScore").textContent).toMatch(/Current Score: 1/);
-    expect(screen.getByRole("currentScore").textContent).not.toMatch(/Current Score: 2/);
-
+    expect(screen.getByTestId("currentScore").textContent).toMatch(/Current Score: 1/);
+    expect(screen.getByTestId("currentScore").textContent).not.toMatch(/Current Score: 2/);
   });
 
   it('renders highScore passed as props', () => {
     render(<ScoreBoard highScore={9} />);
     
-    expect(screen.getByRole("highScore").textContent).toMatch(/High Score: 9/);
-    expect(screen.getByRole("highScore").textContent).not.toMatch(/High Score: 1/);
-
+    expect(screen.getByTestId("highScore").textContent).toMatch(/High Score: 9/);
+    expect(screen.getByTestId("highScore").textContent).not.toMatch(/High Score: 1/);
   });
-
 });
-
-
-// describe("App component", () => {
-//     it("renders correct heading", () => {
-//       render(<App />);
-//       expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
-//     });
-//   });
