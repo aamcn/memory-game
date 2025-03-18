40e70ffb993f5d7ae39c130f88fd175aa81e7354
malcomcrab <malcomcrabesq@outlook.com>
2025-03-18 16:05:54 +0000

    Test: adds test file for ramdomNumber module.

--- src/modules/randomNumber.test.js
+++ src/modules/randomNumber.test.js
@@ -2,7 +2,18 @@ import { it, describe, expect} from 'vitest';
 import { getRandomInt } from './randomNumber';
 
 describe('getRandomInt', () => {
-  it('Returns a number', () => {
+  
+    it('Should return a number', () => {
     expect(getRandomInt(10)).toBeTypeOf("number")  
   });
+
+  it("Should return a number smaller than the input argument", () => {
+    expect(getRandomInt(10)).toBeLessThan(11)
+  })
+
+  it("Should return a number greater than 0", () => {
+    expect(getRandomInt(10)).toBeGreaterThan(0)
+  })
+
 });
+
