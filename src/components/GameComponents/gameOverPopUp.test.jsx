8015cc5456e4ccc0895eb699b709dad0d5446641
malcomcrab <malcomcrabesq@outlook.com>
2025-03-19 15:57:48 +0000

    Test: Adds further testing to GameOverPopUp component

--- src/components/GameComponents/gameOverPopUp.test.jsx
+++ src/components/GameComponents/gameOverPopUp.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
+import userEvent from "@testing-library/user-event";
 import GameOverPopUp from "./GameOverPopUp";
 
 
@@ -20,11 +21,24 @@ describe('Retry Button', () => {
 
     it('Should render "Retry" button', () => {
         render(<GameOverPopUp />)
-    
         const retryButton = screen.getByRole("button", {name: "Retry?"});
         expect(retryButton).toBeInTheDocument();
     })
 
-    
+    it('Should change "gameResults" and "gameStarted" to "true" when clicked', async () => {
+        
+        const user = userEvent.setup();
+        let gameStarted = false;
+        let gameResults = false;
+        const setGameStarted = () => {gameStarted = true}
+        const setGameResults = () => {gameResults = true}
+        
+        render(<GameOverPopUp setGameResults={setGameResults} setGameStarted={setGameStarted} gameresults={gameResults} gameStarted={gameStarted}/>)
+            const retryButton = screen.getByRole("button", {name: "Retry?"});
+            await user.click(retryButton);
+        
+        expect(gameResults).toBe(true);
+        expect(gameStarted).toBe(true)
+    })
 
 })
