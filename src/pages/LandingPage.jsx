685517763c0ebfb97aea2a81bdf51a199dd2f0c6
malcomcrab <malcomcrabesq@outlook.com>
2025-03-20 14:52:38 +0000

    Style: Adds CSS styling to LandingPage and game components

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
-
+import styles from "../cssModules/landingPage.module.css"
 function LandingPage() {
   return (
-    <>
-      <div>
-        <img width="400px" src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYm9qM2dmeW42aHQxMXAwNnA2YmV1dTRiaGoweHE4YWZoMDJoZnJkeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/teYrgUtS8tPl6/giphy.gif"></img>
+    <div className={styles.mainContainer}>
+      <div className={styles.imageContainer}>
+        <img className={styles.mainImage} width="400px" src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYm9qM2dmeW42aHQxMXAwNnA2YmV1dTRiaGoweHE4YWZoMDJoZnJkeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/teYrgUtS8tPl6/giphy.gif"></img>
       </div>
       <div >
-        <h1 >Poke-Mem</h1>
+        <h1 className={styles.mainHeading}>Poke-Mem</h1>
       </div>
-      <div>
-        <Link to="/game-page">Enter</Link>
+      <div className={styles.startButton}>
+        <Link to="/game-page">Start</Link>
       </div>
-    </>
+    </div>
   );
 }
 
