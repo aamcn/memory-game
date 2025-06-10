ca7802bf502185189bd0a4a9a19edf32f5c6d906
malcomcrab <malcomcrabesq@outlook.com>
2025-06-10 10:52:55 +0100

    Build: install github pages and configure for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/memory-game",
   test: {
     globals: true,
     environment: "jsdom",
