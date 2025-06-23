b17669e65e0746a510c6488a6cae821c935b9ec5
aaronmcnulty <malcomcrabesq@outlook.com>
2025-06-23 17:23:31 +0100

    Test: Update GameOverPopUp test to use updated variable naming

--- src/components/gamePageComponents/gameOverPopUp/gameOverPopUp.test.jsx
+++ src/components/gamePageComponents/gameOverPopUp/gameOverPopUp.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import userEvent from "@testing-library/user-event";
 import GameOverPopUp from "./GameOverPopUp";
+import React from "react";
 
 describe("GameOverPopUp Component", () => {
   it("Should render GameOverPopUp", () => {
@@ -23,26 +24,26 @@ describe("Retry Button", () => {
   it('Should change "gameResults" and "gameStarted" to "true" when clicked', async () => {
     const user = userEvent.setup();
     let gameStarted = false;
-    let gameResults = false;
+    let gameOver = false;
     const setGameStarted = () => {
       gameStarted = true;
     };
-    const setGameResults = () => {
-      gameResults = true;
+    const setGameOver = () => {
+      gameOver = true;
     };
 
     render(
       <GameOverPopUp
-        setGameResults={setGameResults}
+        setGameOver={setGameOver}
         setGameStarted={setGameStarted}
-        gameresults={gameResults}
+        gameOver={gameOver}
         gameStarted={gameStarted}
       />,
     );
     const retryButton = screen.getByRole("button", { name: "Retry?" });
     await user.click(retryButton);
 
-    expect(gameResults).toBe(true);
+    expect(gameOver).toBe(true);
     expect(gameStarted).toBe(true);
   });
 });
