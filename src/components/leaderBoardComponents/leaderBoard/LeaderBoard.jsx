e3b5ca70430295578743eca69e522acc5dc57489
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-01 15:13:57 +0100

    Feat: Add LeaderBoardForm component

--- src/components/leaderBoardComponents/leaderBoard/LeaderBoard.jsx
+++ src/components/leaderBoardComponents/leaderBoard/LeaderBoard.jsx
@@ -1,13 +1,14 @@
 import React from "react"
-
+import { useState } from "react"
 
 function LeaderBoard(){
+const [leaderBoard, setLeaderBoard] = useState(null)
+
+
 
     return(
         <div>
-            <div>
-                
-            </div>
+            
         </div>
     )
 }
---
+++ src/components/leaderBoardComponents/leaderBoardForm/LeaderBoardForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LeaderBoardForm(){
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default LeaderBoardForm; 
