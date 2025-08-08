c7084eda02ffad0de69e8693f3565be9623aad71
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-08 18:29:45 +0100

    Test: checkForWin.js: Add graceful error handling and add test suite and tests

--- src/modules/checkForWin/checkForWin.js
+++ src/modules/checkForWin/checkForWin.js
@@ -1,4 +1,9 @@
 export function checkForWin(currentScore, cardTotal, setGameWon) {
+  
+  if (typeof currentScore !== 'number' || typeof cardTotal !== 'number') {
+    throw new Error("Invalid input, both currentScore and cardTotal must be numbers");
+  }
+  
   if (currentScore === cardTotal) {
     setGameWon(true);
   }
---
+++ src/modules/checkForWin/checkForWin.test.js
@@ -0,0 +1,36 @@
+import { checkForWin } from "./checkForWin";
+import { it, describe, expect, vi, beforeEach } from "vitest";
+describe("checkForWin", () => {
+
+    beforeEach(() => {  
+        vi.clearAllMocks();
+        
+    });
+
+    const cardTotal = 4 // Total number of cards in the game.
+
+    it("should call setGameWon when a winning condition is met", () => {
+       const mockSetGameWon = vi.fn(() => {});
+       checkForWin(4, cardTotal, mockSetGameWon)
+       expect(mockSetGameWon).toHaveBeenCalled();
+    })
+
+    it("should call setGameWon when a winning condition is met", () => {
+       const mockSetGameWon = vi.fn(() => {});
+       checkForWin(4, cardTotal, mockSetGameWon)
+       expect(mockSetGameWon).toHaveBeenCalledWith(true);
+    })
+
+    it("should NOT call setGameWon when a winning condition is met", () => {
+        const mockSetGameWon = vi.fn(() => {});
+        checkForWin(1, cardTotal, mockSetGameWon)
+       expect(mockSetGameWon).not.toHaveBeenCalled();
+    })
+
+    it("should NOT call setGameWon and pass 'false' when a winning condition is met", () => {
+        const mockSetGameWon = vi.fn(() => {});
+        checkForWin(1, cardTotal, mockSetGameWon)
+       expect(mockSetGameWon).not.toHaveBeenCalledWith(false);
+    })
+
+});
