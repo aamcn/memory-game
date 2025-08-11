0f1428722bdb96fecefcd3fa01e2b8875a3f2c42
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-11 14:03:22 +0100

    refactor: Button.jsx to render className and aria-label from props and add new props to propTypes.

--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -1,19 +1,21 @@
 import PropTypes from "prop-types";
+import React from "react";
 
-function Button({ buttonId, buttonText, buttonType, onClick }) {
+
+function Button({ buttonId, buttonText, buttonType, onClick, buttonClass }) {
   return (
-    <>
-      <button id={buttonId} type={buttonType} onClick={onClick}>
+      <button id={buttonId} type={buttonType} onClick={onClick} aria-label={buttonText} className={buttonClass}>
         {buttonText}
       </button>
-    </>
   );
 }
 
 Button.propTypes = {
-  buttonId: PropTypes.number,
+  buttonId: PropTypes.string,
   buttonText: PropTypes.string,
   buttonType: PropTypes.string,
+  buttonClass: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
