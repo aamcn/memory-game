9a0b8eba390f645c11263b032cb243bb52bbdda0
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-12 18:36:13 +0100

    Refactor: Move PokemonUrlGenerator function to seperate module for export

---
+++ src/modules/pokemonUrlGenerator/PokemonUrlGenerator.js
@@ -0,0 +1,22 @@
+import { getRandomInt } from "../randoNumber/randomNumber";
+
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+  /* Resets chosenPokemon and pokeApiUrls to original value.
+     Creates and stores random pokemon api urls by appending the apiUrl
+     with a random number. if a duplicate url is created it is disregarded.
+  */
+  function generatePokemonUrls(cardTotal) {
+   
+    let pokeApiUrls = [];
+    while (pokeApiUrls.length < parseInt(cardTotal)) {
+      let randomNumber = getRandomInt(1, 150);
+      if (!pokeApiUrls.includes(apiUrl + randomNumber)) {
+        pokeApiUrls.push(apiUrl + randomNumber);
+      }
+    }
+    // When pokeApiUrls length equals the cardTotal the fetchPokeUrls function is called.
+    return pokeApiUrls
+  }
+
+  export { generatePokemonUrls };
