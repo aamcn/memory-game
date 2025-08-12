dc142d61f89b8d6ae9b5dd2af628137b73d5ea44
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-12 18:35:29 +0100

    Refactor: Move fetchPokeUrls function to separate module for import.

---
+++ src/modules/fetchPokeUrls/fetchPokeUrls.js
@@ -0,0 +1,14 @@
+
+// Maps and fetches pokemon API URLs and inserts the returned data into chosenPokemon state.
+  function fetchPokeUrls(pokeApiUrls, setChosenPokemon) {
+    pokeApiUrls.map((pokemonUrl) => {
+      fetch(pokemonUrl, { mode: "cors" })
+        .then((response) => response.json())
+        .then((response) =>
+          setChosenPokemon((chosenPokemon) => [...chosenPokemon, response]),
+        )
+        .catch((error) => console.error(error));
+    });
+  }
+
+  export { fetchPokeUrls };
