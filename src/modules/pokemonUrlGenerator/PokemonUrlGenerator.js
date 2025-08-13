779df4c2ba919d61eb20cc6e08184124f0c65262
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-13 12:59:41 +0100

    Test: Add test suite and testing for PokemonUrlGenerator

--- src/modules/pokemonUrlGenerator/PokemonUrlGenerator.js
+++ src/modules/pokemonUrlGenerator/PokemonUrlGenerator.js
@@ -1,18 +1,17 @@
-import { getRandomInt } from "../randoNumber/randomNumber";
+import { getRandomInt } from "../randomNumber/randomNumber";
 
-const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+const apiUrlPrefix = "https://pokeapi.co/api/v2/pokemon/";
 
   /* Resets chosenPokemon and pokeApiUrls to original value.
      Creates and stores random pokemon api urls by appending the apiUrl
      with a random number. if a duplicate url is created it is disregarded.
   */
   function generatePokemonUrls(cardTotal) {
-   
     let pokeApiUrls = [];
     while (pokeApiUrls.length < parseInt(cardTotal)) {
       let randomNumber = getRandomInt(1, 150);
-      if (!pokeApiUrls.includes(apiUrl + randomNumber)) {
-        pokeApiUrls.push(apiUrl + randomNumber);
+      if (!pokeApiUrls.includes(apiUrlPrefix + randomNumber)) {
+        pokeApiUrls.push(apiUrlPrefix + randomNumber);
       }
     }
     // When pokeApiUrls length equals the cardTotal the fetchPokeUrls function is called.
---
+++ src/modules/pokemonUrlGenerator/pokemonUrlGenerator.test.js
@@ -0,0 +1,46 @@
+import { generatePokemonUrls } from "./PokemonUrlGenerator";
+import { describe, it, expect} from "vitest";
+
+
+const mockApiUrlPrefix = "https://pokeapi.co/api/v2/pokemon/";
+
+describe("generatePokemonUrls", () => {
+
+    it("should generate expected number of unique Pokemon API URLs", () => {
+    const cardTotal = 5;
+    const urls = generatePokemonUrls(cardTotal);
+    expect(urls.length).toBe(cardTotal);
+  });
+
+  it("should generate api urls with the correct prefix", () => {
+    const cardTotal = 5;
+    const urls = generatePokemonUrls(cardTotal);
+    urls.forEach((url) => {
+      expect(url).toContain(mockApiUrlPrefix);
+    });
+  });
+
+  it("should generate unique Pokemon API URLs", () => {
+    const cardTotal = 5;
+    const urls = generatePokemonUrls(cardTotal);
+    const uniqueUrls = [...new Set(urls)];
+    expect(uniqueUrls.length).toBe(cardTotal);
+  });
+
+  
+  it("Each url should end with a number 0 or greater", () => {
+    const cardTotal = 5;
+    const urls = generatePokemonUrls(cardTotal);
+    const splitUrls = urls.map(url => {
+      const splitUrlArray =  url.split("/");
+      return splitUrlArray[splitUrlArray.length - 1];
+    });
+    splitUrls.forEach((url) => {
+      expect(parseInt(url)).not.toBeNaN();
+      expect(parseInt(url)).toBeTypeOf('number');
+       expect(Number(url)).toBeGreaterThanOrEqual(0);
+        expect(Number(url)).toBeLessThanOrEqual(150);
+    });
+  });
+
+});
