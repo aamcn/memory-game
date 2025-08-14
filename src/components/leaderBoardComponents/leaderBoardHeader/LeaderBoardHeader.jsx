6d7397bbbb7bddbfd3b28fc12bfbd4607c40dc57
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-14 15:54:57 +0100

    Refactor LeaderBoardHeader to update title with props and include accesibility to html

--- src/components/leaderBoardComponents/leaderBoardHeader/LeaderBoardHeader.jsx
+++ src/components/leaderBoardComponents/leaderBoardHeader/LeaderBoardHeader.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import styles from "./leaderBoardHeader.module.css"
-function LeaderBoardHeader(){
-    
-    return(
-        <div className={styles.leaderBoardHeader}>
-            <h1>Leader Board</h1>
-        </div>
-    )
+import styles from "./leaderBoardHeader.module.css";
+import PropTypes from "prop-types";
+
+function LeaderBoardHeader({ selectedDifficulty }) {
+  return (
+    <div
+      className={styles.leaderBoardHeadContainer}
+      data-testid="leader-board-header"
+    >
+      <h1
+        className={styles.leaderBoardTitle}
+        aria-label={`${selectedDifficulty} Leader Board`}
+      >
+        {selectedDifficulty} Leader Board
+      </h1>
+    </div>
+  );
 }
+
+LeaderBoardHeader.propTypes = {
+  selectedDifficulty: PropTypes.string.isRequired,
+};
 
 export default LeaderBoardHeader;
