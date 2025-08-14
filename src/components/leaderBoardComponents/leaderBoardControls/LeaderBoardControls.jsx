e18036cec1d5296479827c4a8925667559093530
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-14 15:51:24 +0100

    Feat: Refactor leaderBoardControls to update difficulty option to state and render higher standard of html

--- src/components/leaderBoardComponents/leaderBoardControls/LeaderBoardControls.jsx
+++ src/components/leaderBoardComponents/leaderBoardControls/LeaderBoardControls.jsx
@@ -1,18 +1,55 @@
-import styles from "./leaderBoardControls.module.css"
+import styles from "./leaderBoardControls.module.css";
+import PropTypes from "prop-types";
 import React from "react";
 
-function LeaderBoardControls(){
+function LeaderBoardControls({ setSelectedDifficulty, selectedDifficulty }) {
+  const handleDifficultyChange = (e) => {
+    setSelectedDifficulty(e.target.value);
+  };
 
-    return(
-        <div className={styles.leaderBoardControls}>
-            <label>Leader Boards</label>
-            <select>
-                <option>Easy</option>
-                <option>Medium</option>
-                <option>Hard</option>
-            </select>
-        </div>
-    )
+  return (
+    <div className={styles.leaderBoardControls} data-testid="leaderboard-controls-container">
+      <label className={styles.leaderBoardLabel} htmlFor="difficulty-select">
+        Difficulty Selector
+      </label>
+      <select
+        id="difficulty-select"
+        value={selectedDifficulty}
+        onChange={handleDifficultyChange}
+        className={styles.leaderBoardSelect}
+        aria-label="Difficulty Selector"
+        data-testid="difficulty-selector"
+      >
+        <option
+          value="Easy"
+          className={styles.leaderBoardDifficulty}
+          aria-label="Easy Difficulty"
+        >
+          Easy
+        </option>
+        <option
+          value="Medium"
+          className={styles.leaderBoardDifficulty}
+          aria-label="Medium Difficulty"
+          data-testid="medium-option"
+        >
+          Medium
+        </option>
+        <option
+          value="Hard"
+          className={styles.leaderBoardDifficulty}
+          aria-label="Hard Difficulty"
+        >
+          Hard
+        </option>
+      </select>
+    </div>
+  );
 }
 
-export default LeaderBoardControls
+LeaderBoardControls.propTypes = {
+  setSelectedDifficulty: PropTypes.func.isRequired,
+  selectedDifficulty: PropTypes.string.isRequired,
+};
+
+export default LeaderBoardControls;
