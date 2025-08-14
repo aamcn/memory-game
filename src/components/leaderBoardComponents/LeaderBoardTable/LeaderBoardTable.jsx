6b1b66fbb6e6da9337ce6ac10b746558adcf6052
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-14 17:11:44 +0100

    Feat: Leaderboard maps and displays leaderbaord data passed as props.

---
+++ src/components/leaderBoardComponents/LeaderBoardTable/LeaderBoardTable.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+import styles from './leaderBoard.module.css'
+import PropTypes from "prop-types";
+
+
+function LeaderBoardTable({ sortedLeaderBoardData }) {
+
+  
+
+    return(
+        <div className={styles.leaderBoardContainer} data-testid="leaderboard-container">
+            <table className={styles.leaderBoardTable} aria-label="Leader Board Table">
+                <thead className={styles.leaderBoardHeader}>
+                    <tr className={styles.leaderBoardRow}>
+                        <th className={styles.leaderBoardCell} aria-label="Leaderboard Position">Position</th>
+                        <th className={styles.leaderBoardCell} aria-label="Player Name">Player Name</th>
+                        <th className={styles.leaderBoardCell} aria-label="Finish Time">Finish Time</th>
+                    </tr>
+                </thead>
+                <tbody className={styles.leaderBoardBody} >
+                    {sortedLeaderBoardData.map((entry, index) => (
+                        <tr key={entry.id} className={styles.leaderBoardRow} data-testid={`row-${index + 1}`} aria-label={`Row ${index + 1}`}>
+                            <td className={styles.leaderBoardCell} aria-label={`Leaderboard Position ${index + 1}`}>{index + 1}</td>
+                            <td className={styles.leaderBoardCell} aria-label={`Player Name ${entry.name}`}>{entry.name}</td>
+                            <td className={styles.leaderBoardCell} aria-label={`Finish Time ${entry.time}`}>{entry.time}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+LeaderBoardTable.propTypes = {
+    sortedLeaderBoardData: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            time: PropTypes.string.isRequired
+        })
+    ).isRequired
+};
+
+export default LeaderBoardTable; 
