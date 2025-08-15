80bee0b186d0352d2040e7f5daf134146dd17c72
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-15 15:42:48 +0100

    Refactor: sortByTime.js module to use correct variable names passed in.

--- src/modules/sortByTime/sortByTime.js
+++ src/modules/sortByTime/sortByTime.js
@@ -1,7 +1,7 @@
 // Sorts leaderboard data by finish time
 function sortByTime(a, b) {
-        const timeA = parseInt(a.finish_time) || 0;
-        const timeB = parseInt(b.finish_time) || 0;
+        const timeA = a.finish_time.replaceAll(":", "");
+        const timeB = b.finish_time.replaceAll(":", "");
         return timeA - timeB;
     }
 
