0c12fd33f308d2d387837e1c01fb2d19ce8b89ad
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-18 13:25:50 +0100

    Test: Add test suite and tests to sortByTime module and add validation for inputs

--- src/modules/sortByTime/sortByTime.js
+++ src/modules/sortByTime/sortByTime.js
@@ -1,8 +1,10 @@
 // Sorts leaderboard data by finish time
 function sortByTime(a, b) {
+    if (typeof a.finish_time === "string" && typeof b.finish_time === "string") {
         const timeA = a.finish_time.replaceAll(":", "");
         const timeB = b.finish_time.replaceAll(":", "");
         return timeA - timeB;
     }
+}
 
 export { sortByTime };
