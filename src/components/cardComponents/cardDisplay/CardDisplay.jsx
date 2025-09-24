65a3d9c568cd31ac12c649e27636a3552e7d460a
aaronmcnulty <malcomcrabesq@outlook.com>
2025-09-24 17:01:44 +0100

    Refactor: Redundant useEffect hooks to set isHidden and cardsContainerClassName on render instead

--- src/components/cardComponents/cardDisplay/CardDisplay.jsx
+++ src/components/cardComponents/cardDisplay/CardDisplay.jsx
@@ -1,6 +1,6 @@
 import CardTemplate from "../cardTemplate/CardTemplate";
 import styles from "./cardDisplay.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -14,39 +14,34 @@ function CardDisplay({
   cardTotal,
 }) {
   const [isHidden, setIsHidden] = useState(false);
-  const [cardsContainerClassName, setCardsContainerClassName] = useState(null);
 
-  /*
-    When isHidden is updated to true, it is reverted back to false after 0.5 seconds.
-    This hides the cards while their order is shuffled.
-  */
-  useEffect(() => {
-    if (isHidden != false) {
-      setTimeout(() => {
-        setIsHidden(false);
-      }, 500);
-    }
-  }, [isHidden]);
+  let cardContainerClass = "";
 
-  /*
-    Depending on the variable of cardTotal a different className is set in cardsContainerClassName. 
-    This allows for different styling to be applied to the div depending on the total of cards in play.CardDisplay.
-  */
-  useEffect(() => {
-    if (cardTotal == 9) {
-      setCardsContainerClassName(`${styles.nineCardsContainer}`);
+  // If isHidden is true, set it back to false after 0.5 seconds to re-render the cards.
+  if (isHidden != false) {
+    setTimeout(() => {
+      setIsHidden(false);
+    }, 500);
+  }
+
+  // Set the class name for the cards container based on the cardTotal prop.
+  const setCardClassName = () => {
+    if (cardTotal === 9) {
+      cardContainerClass = `${styles.nineCardsContainer}`;
     }
-    if (cardTotal == 6) {
-      setCardsContainerClassName(`${styles.sixCardsContainer}`);
+    if (cardTotal === 6) {
+      cardContainerClass = `${styles.sixCardsContainer}`;
     }
-    if (cardTotal == 4) {
-      setCardsContainerClassName(`${styles.fourCardsContainer}`);
+    if (cardTotal === 4) {
+      cardContainerClass = `${styles.fourCardsContainer}`;
     }
-  }, [cardTotal]);
+  };
+  // Call the function to set the class name when the component renders.
+  setCardClassName();
 
   return (
     !isHidden && (
-      <div data-testid="cards-container" className={cardsContainerClassName}>
+      <div data-testid="cards-container" className={cardContainerClass}>
         {chosenPokemonCardData &&
           chosenPokemonCardData.map((pokemonCardDetails) => {
             return (
